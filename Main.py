b625f70c2c1b6012da7ca5daacd89e30ce5eac33
huseyin <huseyinsarsilmaz@hotmail.com>
2022-02-27 21:48:16 +0300

    First tile added

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ Config.set('graphics', 'height', '400')
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty,Clock
-from kivy.graphics.vertex_instructions import Line
+from kivy.graphics.vertex_instructions import Line,Quad
 from kivy.core.window import Window
 from kivy import platform
 class MainWidget(Widget):
@@ -18,8 +18,12 @@ class MainWidget(Widget):
         self.offsetY = 0
         self.offsetX = 0
         self.speedX = 0
+        self.tile = None
+        self.tix = 0
+        self.tiy = 0
         self.createVerticalLines()
         self.createHorizontalLines()
+        self.createTiles()
         if(platform in ("linux","win","macosx")):
             self._keyboard = Window.request_keyboard(self.keyboard_closed, self)
             self._keyboard.bind(on_key_down=self.on_keyboard_down)
@@ -51,19 +55,41 @@ class MainWidget(Widget):
     def createHorizontalLines(self):
         with self.canvas:
             for i in range(8): self.horizontalLines.append(Line())
+
+    def createTiles(self):
+        with self.canvas:
+            self.tile = Quad()
     
+    def updateTiles(self):
+        xmin,ymin = self.getTileCoordinates(self.tix,self.tiy)
+        xmax,ymax = self.getTileCoordinates(self.tix+1,self.tiy+1)
+        x1,y1 = self.perspective(xmin,ymin)
+        x2,y2 = self.perspective(xmin,ymax)
+        x3,y3 = self.perspective(xmax,ymax)
+        x4,y4 = self.perspective(xmax,ymin)
+        self.tile.points = [x1,y1,x2,y2,x3,y3,x4,y4]
+
     def getLineXfromIndex(self,index):
         center = self.perspectivePointX
         spacing = self.width * 0.25
         offset = index - 0.5
         lineX = center + spacing*offset + self.offsetX
         return lineX
+
+    def getLineYfromIndex(self,index):
+        y = index*0.1*self.height-self.offsetY
+        return y
+
+    def getTileCoordinates(self,ix,iy):
+        x = self.getLineXfromIndex(ix)
+        y = self.getLineYfromIndex(iy)
+        return x,y
     
     def updateHorizontalLines(self):
         minX = self.getLineXfromIndex(-4)
         maxX = self.getLineXfromIndex(5)
         for i in range(8): 
-            y = i*0.1*self.height-self.offsetY
+            y = self.getLineYfromIndex(i)
             x1,y1 = self.perspective(minX,y)
             x2,y2 = self.perspective(maxX,y)
             self.horizontalLines[i].points = [x1,y1,x2,y2]
@@ -92,6 +118,7 @@ class MainWidget(Widget):
     def update(self,dt):
         self.updateVerticalLines()
         self.updateHorizontalLines()
+        self.updateTiles()
         self.offsetY += 1 * dt * 60
         self.offsetX += self.speedX * dt * 60
         if(self.offsetY >= 0.1 * self.height) : self.offsetY -= 0.1 * self.height
